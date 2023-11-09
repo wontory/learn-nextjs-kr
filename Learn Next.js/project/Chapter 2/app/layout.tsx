7287f6871873df2bfd58dada0f27752a33518896
조성원 <tjddnjsjo@kakao.com>
2023-11-09 12:25:06 +0900

    feat: Create Next.js project Chapter 2

---
+++ Learn Next.js/project/Chapter 2/app/layout.tsx
@@ -0,0 +1,13 @@
+import '@/app/ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
