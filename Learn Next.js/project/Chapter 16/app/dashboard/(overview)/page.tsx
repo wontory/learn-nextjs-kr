b6c8f71a4bc1fdc3e2170cb4b462590ab866e5ed
조성원 <tjddnjsjo@kakao.com>
2023-12-08 22:46:23 +0900

    refactor: Metadata 수정

--- Learn Next.js/project/Chapter 16/app/dashboard/(overview)/page.tsx
+++ Learn Next.js/project/Chapter 16/app/dashboard/(overview)/page.tsx
@@ -8,6 +8,11 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
--- Learn Next.js/project/Chapter 16/app/dashboard/layout.tsx
+++ Learn Next.js/project/Chapter 16/app/dashboard/layout.tsx
@@ -1,9 +1,4 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Dashboard',
-};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
